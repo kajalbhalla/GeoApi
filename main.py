8b53a1d594fd377724e1f0d605838e4ad56861ab
kajalbhalla <75016607+kajalbhalla@users.noreply.github.com>
2020-11-25 21:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ app = Flask(__name__)
 
 
 @app.route('/')
-
-    """Return a friendly HTTP greeting."""
 gmaps=googlemaps.Client(key='AIzaSyD9zIRT7YEXqFymHsxUEN4U18BLZYeW4hY')
 geocode_result=gmaps.geocode('1600 Amphitheatre Parkway,CA')
 print(geocode_result)
