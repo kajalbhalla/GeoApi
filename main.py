6ecdf34bdf404504b6d2bb7ec3e2797eaf129673
kajalbhalla <75016607+kajalbhalla@users.noreply.github.com>
2020-11-26 18:54:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def apig():
-    gmaps=googlemaps.Client(key='AIzaSyD9zIRT7YEXqFymHsxUEN4U18BLZYeW4hY')
-    geocode_result=gmaps.geocode('1600 Amphitheatre Parkway,CA')
-    return geocode_result['results'][0]['geometry']['location']
+    api_key='AIzaSyD9zIRT7YEXqFymHsxUEN4U18BLZYeW4hY'
+    url='https://maps.googleapis.com/maps/api/geocode/json?'
+    place=['AV JUAREZ 2318 405+LA PAZ+PUEBLA+PUE+72160','MINEROS 54+COL REAL DEL MONTE+ALVARO OBREGON+CDMX+01130']
+    lat1=[]
+    lng1=[]
+    for i in range(1):
+        res_ob=requests.get(url+'address='+place[i]+'&key='+api_key)
+        x=res_ob.json()
+        lat1[i]=str(x['results'][0]['geometry']['location'][lat])
+        lng1[i]=str(x['results'][0]['geometry']['location'][lng])
+    return lat1+lng1
+    
     
   
     
