2aed14c407a1b735cfe954daf0a46cb2a37c597d
kajalbhalla <75016607+kajalbhalla@users.noreply.github.com>
2020-11-27 19:08:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def apig():
           'HIDALGO 59+CENTRO+VILLA GUERRERO+EM+51760','MALECON 26+ACUEDUCTO DE GUADALUPE+GUSTAVO A.MADERO+CDMX+07270','CALENDULA 84+XOTEPINGO+COYOACAN+CDMX+04610',
           'OAXACA 37 INT 3+ROMA+CUAUHTEMOC+CDMX+06700','AV JUAREZ 5107+POPULAR+CHIHUAHUA+CHI+31040','MORENA 206+DEL VALLE+BENITO JUAREZ+CDMX+03100',
            'VILLA JARDIN 118+VILLAS JARDIN+CELAYA+GTO+38035']
-    lat1 = [None] * 10
-    lng1 = [None] * 10
+    lat1 = []
+    lng1 = []
     for i in range(10):
         res_ob=requests.get(url+'address='+place[i]+'&key='+api_key)
         x=res_ob.json()
-        lat1[i]=str(x['results'][0]['geometry']['location']['lat'])
-        lng1[i]=str(x['results'][0]['geometry']['location']['lng'])
+        lat1.append(str(x['results'][0]['geometry']['location']['lat']))
+        lng1.append(str(x['results'][0]['geometry']['location']['lng']))
     listToStr1 = ' '.join([str(elem) for elem in lat1])
     listToStr2 = ' '.join([str(ele) for ele in lng1]) 
     return listToStr1+listToStr2
