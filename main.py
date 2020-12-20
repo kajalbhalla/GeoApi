5a462ae33621c810fa73766d5da7097a9ebad38e
kajalbhalla <75016607+kajalbhalla@users.noreply.github.com>
2020-12-20 22:27:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def apig():
-    place=['Monte Aventino 134+Fuentes del Valle+SAN PEDRO GARZA GARCIA+NL+66220',
-'CERRADA FRESNOS 44+VILLAS SANTORINI+SACRAMENTO+COA+27750',
-'2DA CDA DE OLIVO 13+FLORIDA+ALVARO OBREGON+CDMX+01030',
-'ALLENDE 2609+ZARCO+CHIHUAHUA+CHI+31020']
-
+    place=['TURQUEZA 160 COTO 3+BONANZA RESIDENCIAL+TLAJOMULCO DE ZUNIGA+JAL+45645',
+'CALZ LA VIRGEN 3000 36 16+UH CULHUACAN STUNAM+COYOACAN+CDMX+04908',
+'XOCHICALCO 39 4DEP 02+NARVARTE+BENITO JUAREZ+CDMX+03020',
+'MAYASA 1968+COLOMOS PROVIDENCIA+GUADALAJARA+JAL+44660',
+'CLL ADOLFO LUIS CORTINEZ NO 9+COL PROGRESISTA+IZTAPALAPA+CDMX+09240',
+'AGRONOMOS 404 INT 5D+TECNOLOGICO+MONTERREY+NL+64700',
+'C MALAGA 205+FRACC VINEDOS+DELICIAS+CHI+33068']
 
 
 
 
     api_key="AIzaSyD9zIRT7YEXqFymHsxUEN4U18BLZYeW4hY"
     url="https://maps.googleapis.com/maps/api/geocode/json?"
-    lat1=[None] * 4
-    lng1=[None] * 4
-    for i in range(4):
+    lat1=[None] * 7
+    lng1=[None] * 7
+    for i in range(7):
         res_ob=requests.get(url+'address='+place[i]+'&key='+api_key)
         x=res_ob.json()
         #lat1.append(str(x['results'][0]['geometry']['location']['lat']))
